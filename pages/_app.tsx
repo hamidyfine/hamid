c757c7a31456c5c751ad134197df20491350efba
Hamid Yaftian <hamid@deriv.com>
2023-03-31 10:44:48 +0800

    adding tailwindcss, chakraUI, ESLint and StyleLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import '@fontsource/ubuntu';
+import '@/styles/globals.scss';
+
+const theme = extendTheme({
+    fonts: {
+        heading: '\'Ubuntu\', sans-serif',
+        body   : '\'Ubuntu\', sans-serif',
+    },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
---
+++ stylelint.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    extends: ['stylelint-config-hudochenkov/full'],
+    plugins: ['stylelint-scss', 'stylelint-order'],
+    rules  : {
+        indentation                : 4,
+        'color-hex-case'           : 'upper',
+        'color-hex-length'         : 'long',
+        'no-descending-specificity': null,
+        'at-rule-no-unknown'       : [
+            true,
+            {
+                ignoreAtRules: ['function', 'if', 'each', 'else', 'include', 'mixin', 'return', 'for', 'use', 'forward', 'debug', 'warn', 'extend', 'tailwind', 'apply'],
+            },
+        ],
+        'font-family-no-missing-generic-family-keyword': null,
+        'selector-pseudo-element-no-unknown'           : [
+            true,
+            {
+                ignorePseudoElements: ['v-deep'],
+            },
+        ],
+        'declaration-no-important'   : true,
+        'selector-max-type'          : 3,
+        'selector-max-id'            : 1,
+        'selector-no-qualifying-type': [
+            true,
+            {
+                ignore: ['attribute', 'class', 'id'],
+            },
+        ],
+        'unit-no-unknown': [
+            true,
+            {
+                ignoreUnits: ['xs', 'sm', 'md', 'lg', 'xl'],
+            },
+        ],
+    },
+    syntax: 'scss',
+};
