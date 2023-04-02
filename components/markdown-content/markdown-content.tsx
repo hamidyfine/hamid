6e7ed74ef7c43a7ccf9316aa07317a3edcfc7d71
Hamid Yaftian <hamid@deriv.com>
2023-04-02 13:24:27 +0800

    adopt markdown texts with dark mode

--- components/markdown-content/markdown-content.tsx
+++ components/markdown-content/markdown-content.tsx
@@ -1,15 +1,20 @@
+import { useColorMode } from '@chakra-ui/react';
+import classNames from 'classnames';
+
 type Props = {
   content?: string
 }
 
 const MarkdownContent = ({ content }: Props) => {
+    const { colorMode } = useColorMode();
+
     if (!content) {
         return null;
     }
 
     return (
         <div
-            className='prose'
+            className={classNames('prose', { 'dark:prose-invert': colorMode === 'dark' })}
             dangerouslySetInnerHTML={{ __html: content }}
         />
     );
